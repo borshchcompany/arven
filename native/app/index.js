362f7265563699bb8f72ae10e74a44732d68efaa
Gennadiy Furduy <gennadiy@furduy.com>
2018-02-07 21:00:52 +0200

    Added Redux and redux-thunk

---
+++ native/app/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { StyleSheet, Text, View } from "react-native";
+
+import store from "./configs/store";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<View style={styles.container}>
+					<Text>Start Arven project!</Text>
+				</View>
+			</Provider>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	}
+});
